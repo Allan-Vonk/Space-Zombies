2097174b62a1fb9f73ad3f429d342700a800f292
AlexV03 <78423170+AlexV03@users.noreply.github.com>
2021-02-03 15:08:43 +0100

    Health
    lives

---
+++ Assets/Alex/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Health
+{
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    protected override void CheckHealth()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Kill();
+        }
+
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+    }
+
+    public override void Kill()
+    {
+        base.Kill();
+        //Play explosion after rotation
+        Debug.Log("YOU DEAD");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Zombie"))
+        {
+            ChangeHealth(-1f);
+        }
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public override float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+}
