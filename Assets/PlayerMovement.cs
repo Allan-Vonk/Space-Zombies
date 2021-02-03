8c093cd2a3e1323c157c947b756751f51b91baa4
SeasterSwe <Jakobbulow01@gmail.com>
2021-02-03 09:46:56 +0100

    Rotatate to look at center

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,19 +7,25 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     public float launchForce = 10;
     bool canJump = true;
+    LineRenderer line;
+    public Transform target;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        line = GetComponent<LineRenderer>();
     }
     void Start()
     {
-        
+        line.positionCount = 2;
     }
 
 
     void Update()
     {
+        Vector2 center = target.position - transform.position;
+        var angle = Mathf.Atan2(center.y, center.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         if (Input.GetMouseButtonDown(0) && canJump)
         {
             canJump = false;
@@ -27,10 +33,14 @@ public class PlayerMovement : MonoBehaviour
 
             Vector2 heading = direction();
 
-            var angle = Mathf.Atan2(heading.y, heading.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-
             rb.AddForce(heading / heading.magnitude * launchForce, ForceMode2D.Impulse);
+        }
+        if (canJump)
+        {
+            Vector2 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, mousePos);
         }
     }
 
@@ -38,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        Vector2 heading = mousePos - (Vector2)transform.position;      
+        Vector2 heading = mousePos - (Vector2)transform.position;
         return heading;
     }
     private void OnCollisionEnter2D(Collision2D collision)
