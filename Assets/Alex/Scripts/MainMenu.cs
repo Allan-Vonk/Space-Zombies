c71221ae645de05797eefa85ed45dae1d1fbf16f
AlexV03 <78423170+AlexV03@users.noreply.github.com>
2021-02-03 13:48:33 +0100

    Menu

---
+++ Assets/Alex/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//This will load the level
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();//Quit the game
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");//back to main menu
+    }
+}
